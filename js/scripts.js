314616fe58a9f9b7b44773dc7c43623e0d265e49
Zoe Weinstein <mxzweinstein@gmail.com>
2021-10-31 11:01:17 -0700

    add variables to scripts.js
    Co-authored-by: Patrick Dolan <dolanp1992@gmail.com

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  $("#formOne").SubmitEvent(function() {
+  $("#formOne").submit(function(event) {
+    const person1Input = $("input#person1").val();
+    const person2Input = $("input#person2").val();
+    const animalInput = $("input#animal").val();
+    const exclamationInput = $("input#exclamation").val();
+    const verbInput = $("input#verb").val();
+    const nounInput = $("input#noun").val();
+
     $(".person1").append("blah blah person1");
     $(".person2").append("blah blah person2");
     $(".animal").append("blah blah animal");
@@ -7,5 +14,7 @@ $(document).ready(function() {
     $(".verb").append("blah blah verb");
     $(".noun").append("blah blah noun");
     $("#story").show();
+
+    event.preventDefault();
   });
 });
