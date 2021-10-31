0c919e1eeb83c345fe51e0fb3b7e6109898adc31
Patrick Dolan <dolanp1992@gmail.com>
2021-10-31 15:21:52 -0700

    add logic to take story selection from index.html and display that story when sumbit button is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,34 @@ $(document).ready(function() {
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
-
+    //Story choice Logic
+    const storyChoice = $("#storyChoice").val();
+    const storyNumber = parseInt(storyChoice.charAt(0));
+    //alert(storyNumber);
     $(".person1").append(person1Input);
     $(".person2").append(person2Input);
     $(".animal").append(animalInput);
     $(".exclamation").append(exclamationInput);
     $(".verb").append(verbInput);
     $(".noun").append(nounInput);
-    $("#story").show();
-
+    //Story Display Logic
+    switch(storyNumber) {
+      case 1:
+        $("#story1").show();
+        break;
+      case 2:
+        $("#story2").show();
+        break;
+      case 3:
+        $("#story3").show();
+        break;
+      case 4:
+        $("#story4").show();
+        break;
+      case 5:
+        $("#story5").show();
+        break;
+    }
     event.preventDefault();
   });
 });
