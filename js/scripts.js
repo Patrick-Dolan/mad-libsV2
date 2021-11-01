2a7bd87a458eec66fcca7c48034ae6d3ed5a6adf
Patrick Dolan <dolanp1992@gmail.com>
2021-10-31 19:13:37 -0700

    add button group to switch between stories and a button to reload page

--- js/scripts.js
+++ js/scripts.js
@@ -21,20 +21,50 @@ $(document).ready(function() {
     switch(storyNumber) {
       case 1:
         $("#story1").show();
+        $("#switchStoryGroup").slideDown(100);
         break;
       case 2:
         $("#story2").show();
+        $("#switchStoryGroup").slideDown(100);
         break;
       case 3:
         $("#story3").show();
+        $("#switchStoryGroup").slideDown(100);
         break;
       case 4:
         $("#story4").show();
+        $("#switchStoryGroup").slideDown(100);
         break;
       case 5:
         $("#story5").show();
+        $("#switchStoryGroup").slideDown(100);
         break;
     }
     event.preventDefault();
+  });
+  // Story Switch Logic
+  $("#story1Switch").click(function() {
+    $("#story2,#story3,#story4,#story5").hide();
+    $("#story1").fadeIn(1000);
+  });
+  $("#story2Switch").click(function() {
+    $("#story1,#story3,#story4,#story5").hide();
+    $("#story2").fadeIn(1000);
+  });
+  $("#story3Switch").click(function() {
+    $("#story1,#story2,#story4,#story5").hide();
+    $("#story3").fadeIn(1000);
+  });
+  $("#story4Switch").click(function() {
+    $("#story1,#story2,#story3,#story5").hide();
+    $("#story4").fadeIn(1000);
+  });
+  $("#story5Switch").click(function() {
+    $("#story1,#story2,#story3,#story4").hide();
+    $("#story5").fadeIn(1000);
+  });
+  //Reload page 
+  $("#refreshPageButton").click(function() {
+    location.reload(true);
   });
 });
